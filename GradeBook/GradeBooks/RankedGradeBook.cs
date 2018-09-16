9f1e544ade23b79afc7f4b19386fe7653b52f508
Cuneyt B <cuneytbozyel@gmail.com>
2018-09-16 12:11:04 +0300

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            GradeBookType = GradeBookType.Standard;
+            Type = GradeBookType.Standard;
         }
     }
 }
