f59acc48c6e0d58b8f50f5e62be7f418ffb1e3ce
Cuneyt B <cuneytbozyel@gmail.com>
2018-09-16 22:35:06 +0300

    Completed the module2

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,6 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
@@ -48,7 +47,7 @@ namespace GradeBook.UserInterfaces
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine("{0} is not a sported type of gradebook, please try again.", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
