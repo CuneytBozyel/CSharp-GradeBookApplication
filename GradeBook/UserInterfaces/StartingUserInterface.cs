32db8a0e95d73da3196ac0eacc23959ecc0e4289
Cuneyt B <cuneytbozyel@gmail.com>
2018-09-16 21:59:30 +0300

    "Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use."

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -43,9 +43,9 @@ namespace GradeBook.UserInterfaces
 
             BaseGradeBook gradeBook;
             if (type == "standard")
-                gradeBook = new BaseGradeBook(name);
-            else if (type == "standard")
-                gradeBook = new BaseGradeBook(name);
+                gradeBook = new StandardGradeBook(name);
+            else if (type == "ranked")
+                gradeBook = new RankedGradeBook(name);
             else
             {
                 Console.WriteLine("{0} is not a sported type of gradebook, please try again.", type);
